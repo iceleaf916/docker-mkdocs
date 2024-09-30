cface815ee50d810dd3f6051b1e6b2ceb0f1a82f
yekaisheng <yekaisheng@uniontech.com>
2024-09-30 12:50:02 +0800

    fix setup.py

--- container-files/bootstrap/setup.py
+++ container-files/bootstrap/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='bootstrap',
     version='1.0.0',
-    py_modules=['bootstrap'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click', 'termcolor', 'GitPython', 'python-crontab'
